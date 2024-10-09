17df0cda3069e46156f4303f874eb665fe17c028
melisa <>
2024-10-09 13:13:56 +0200

    introduccion python

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("Hola Mundo");
+# print("Hola Mundo");
+
+#TIPOS DE VARIABLES
+# a = "hola";
+# x = 1;
+# x1 = 1.0;
+# x2 = True;
+# print(type(a));
+# print(type(x));
+# print(type(x1));
+# print(type(x2));
+
+# CONCATENACIONES
+# z = "Hola" + " " + "caracola"
+# print(z)
+
+# b = "Maribel"
+# print(a + " " + b) 
+
+# ENTRADA POR TECLADO
+# name = input('Enter your name')
+# print(name)
+
+# print("Escribe tu nombre")
+# name = input()
+# print(name)
+
+#LECTURA DE NUMEROS POR ENTRADA
+#z = input("Introduce un número ")
+#print(type(z)) #Devuelve un string
+
+#x = int(input("Introduce un número ")) #Definimos el tipo de la variable 'int'
+#print(type(x)) #Devuelve un entero
+
+x = 5
+print('The number is ' + str(x)) #Definimos el tipo de la variable 'str' porque no concatena con integers
