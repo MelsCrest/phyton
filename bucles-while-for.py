5f9a05ce44240a744dabb1604592ba4df6902bec
Melisa <melisacresta@gmail.com>
2024-10-14 10:41:20 +0200

    bucles while, for y foreach

---
+++ bucles-while-for.py
@@ -0,0 +1,40 @@
+# @ BUCLES WHILE
+#EJERCICIO LOOP CON WHILE
+#new_planet = ''
+#planets = []
+
+#while new_planet.lower() != 'done':
+#    if new_planet:
+#        planets.append(new_planet)
+#    new_planet = input('Enter a new planet, or done if done')
+
+#print(planets)
+
+# @ BUCLES FOR
+#countdown = [4, 3, 2, 1, 0]
+#for number in countdown:
+#    print(number)
+#print("Blast off!! 🚀")
+
+# @ BUCLES FOREACH
+#from time import sleep
+
+#countdown = [4, 3, 2, 1, 0]
+
+#for number in countdown:
+#    print(number)
+#    sleep(1)  # Wait 1 second
+#print("Blast off!! 🚀")
+
+# ************************* #
+#EJERCICIO LOOP EN LISTA
+#new_planet = ''
+#planets = []
+
+#while new_planet.lower() != 'done':
+#    if new_planet:
+#        planets.append(new_planet)
+#    new_planet = input('Enter a new planet or done if done')
+
+#for planet in planets:
+#    print(planet)
