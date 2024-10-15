b8a1a370c161a30d92a6c23d7ac6c8ebec6e9161
Melisa <melisacresta@gmail.com>
2024-10-15 10:38:59 +0200

    clases-errores

--- clases.py
+++ clases.py
@@ -1,11 +1,22 @@
 #CREACIÓN DE CLASES
-class Elevator:
-    def __init__(self, starting_floor): #_init_ es el constructor
-        self.make = "The elevator company"
-        self.floor = starting_floor
+# class Elevator:
+#     def __init__(self, starting_floor): #_init_ es el constructor
+#         self.make = "The elevator company"
+#         self.floor = starting_floor
 
-# To create the object
+# # To create the object
 
-elevator = Elevator(1)
-print(elevator.make) # "The Elevator company"
-print(elevator.floor) # 1
+# elevator = Elevator(1)
+# print(elevator.make) # "The Elevator company"
+# print(elevator.floor) # 1
+
+#CORREGIR ERRORES
+# class Car:
+#     def __init__(self): #Añadir 'self' como parámetro
+#         self.color = "Red" # ends up on the object
+#         # make = "Mercedes" # becomes a local variable in the constructor
+#         self.make = "Mercedes" #Error corregido
+
+# car = Car()
+# print(car.color) # "Red"
+# print(car.make) # would result in an error, `make` does not exist on the object
